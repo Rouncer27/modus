da9fe38493ba2ff5ccd1d6cd6bcee090da362835
Trevor Rounce <trevorrounce@trevors-imac-2.lan>
2023-11-02 09:36:30 -0600

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,108 @@ import React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
+import styled from "styled-components"
+import {
+  B1Black,
+  B2Black,
+  B3Black,
+  BtnPrimaryGreen,
+  H1Black,
+  H2Black,
+  H3Black,
+  medWrapper,
+} from "../styles/helpers"
+
 const IndexPage = () => {
   return (
     <Layout>
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Typography>
+        <div className="wrapper">
+          <h1>Aliquam tincidunt mauris eu risus.</h1>
+          <h2>Vestibulum auctor dapibus neque.</h2>
+          <h3>Vivamus vestibulum ntulla nec ante.</h3>
+          <p className="p1">
+            orem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
+            hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque,
+            aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula
+            sollicitudin laoreet viverra, tortor libero sodales leo, eget
+            blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+            potenti.
+          </p>
+          <p className="p2">
+            Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae
+            luctus metus libero eu augue. Morbi purus libero, faucibus
+            adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent
+            elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum
+            volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu
+            pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu,
+            fermentum et, dapibus sed, urna.
+          </p>
+          <p className="p3">
+            Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna
+            a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis
+            libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing
+            varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna.
+            Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget,
+            gravida vitae, ultricies ac, leo. Integer leo pede, ornare a,
+            lacinia eu, vulputate vel, nisl.
+          </p>
+          <div className="buttons">
+            <a href="#">Learn More</a>
+            <button>Learn More</button>
+            <button disabled>Learn More</button>
+          </div>
+        </div>
+      </Typography>
     </Layout>
   )
 }
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
+const Typography = styled.div`
+  .wrapper {
+    ${medWrapper};
+  }
+
+  h1 {
+    width: 100%;
+    ${H1Black};
+  }
+
+  h2 {
+    width: 100%;
+    ${H2Black};
+  }
+
+  h3 {
+    width: 100%;
+    ${H3Black};
+  }
+
+  .p1 {
+    width: 100%;
+    ${B1Black};
+  }
+
+  .p2 {
+    width: 100%;
+    ${B2Black};
+  }
+
+  .p3 {
+    width: 100%;
+    ${B3Black};
+  }
+
+  .buttons {
+    width: 100%;
+
+    a,
+    button {
+      ${BtnPrimaryGreen};
+    }
+  }
+`
+
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
--- src/styles/helpers/buttons.js
+++ src/styles/helpers/buttons.js
@@ -3,18 +3,18 @@ import { fontSizer } from "./index"
 import { css } from "styled-components"
 import fonts from "./fonts"
 
-const Btn1Base = css`
-  ${fontSizer(1.6, 1.8, 76.8, 150, 1.8)};
+const BtnPrimaryBase = css`
+  ${fontSizer(1.6, 2, 76.8, 150, 1.6)};
   display: inline-block;
   padding: 0.5rem 2.5rem;
   border: none;
-  border-radius: 0.25rem;
+  border-radius: 0;
   transition: all 0.3s ease;
   font-family: ${fonts.fontPrimary};
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
-  line-height: 2.14;
+  line-height: 2.2;
   letter-spacing: normal;
   text-align: center;
   text-transform: uppercase;
@@ -30,185 +30,13 @@ const Btn1Base = css`
   }
 `
 
-export const Btn1Navy = css`
-  ${Btn1Base};
-  background: ${colors.colorPrimary};
-  border: solid 0.2rem ${colors.colorPrimary};
+export const BtnPrimaryGreen = css`
+  ${BtnPrimaryBase};
+  background-color: rgba(0, 88, 72, 0.6);
   color: ${colors.white};
 
   &:hover {
-    border-color: ${colors.colorSecondary};
     background: ${colors.colorSecondary};
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1GreyBlue = css`
-  ${Btn1Base};
-  background: ${colors.colorTertiary};
-  border: solid 0.2rem ${colors.colorTertiary};
-  color: ${colors.white};
-
-  &:hover {
-    border-color: ${colors.colorSecondary};
-    background: ${colors.colorSecondary};
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1Grey = css`
-  ${Btn1Base};
-  background: ${colors.colorAccent};
-  border: solid 0.2rem ${colors.colorAccent};
-  color: ${colors.white};
-
-  &:hover {
-    background: ${colors.colorPrimary};
-    border-color: ${colors.colorPrimary};
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1Primary = css`
-  ${Btn1Base};
-  background: transparent;
-  border: solid 0.2rem #154290;
-  color: #154290;
-
-  &:hover {
-    background: #154290;
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1Secondary = css`
-  ${Btn1Base};
-  background: transparent;
-  border: solid 0.2rem #42454a;
-  color: #42454a;
-
-  &:hover {
-    background: #42454a;
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1White = css`
-  ${Btn1Base};
-  background: transparent;
-  border: solid 0.2rem #fff;
-  color: #fff;
-
-  &:hover {
-    background: #fff;
-    color: ${colors.black};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1Success = css`
-  ${Btn1Base};
-  background: transparent;
-  border: solid 0.2rem #28a745;
-  color: #28a745;
-
-  &:hover {
-    background: #28a745;
-    color: ${colors.white};
-  }
-
-  &:focus {
-    outline: 0.4rem solid #003b49;
-    transition: all 0.35s ease-in-out;
-  }
-
-  &:disabled {
-    &:hover {
-      background: ${colors.colorTertiary};
-      color: ${colors.colorPrimary};
-    }
-  }
-`
-
-export const Btn1Danger = css`
-  ${Btn1Base};
-  background: transparent;
-  border: solid 0.2rem #dc3545;
-  color: #dc3545;
-
-  &:hover {
-    background: #dc3545;
     color: ${colors.white};
   }
 
