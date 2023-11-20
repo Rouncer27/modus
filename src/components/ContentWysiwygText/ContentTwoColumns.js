735db2edee5ef0b9fb53ab0c24acddecf6a10f68
Trevor Rounce <trevor@switchbackcreative.ca>
2023-11-20 09:08:14 -0700

    added the new content block

--- src/components/ContentWysiwygText/ContentTwoColumns.js
+++ src/components/ContentWysiwygText/ContentTwoColumns.js
@@ -1,17 +1,111 @@
 import React from "react"
 import styled from "styled-components"
+import {
+  B1Black,
+  H1Black,
+  H2Black,
+  H3Black,
+  colors,
+  medWrapper,
+} from "../../styles/helpers"
 
 const ContentTwoColumns = ({ data }) => {
-  console.log("ContentTwoColumns", data)
   return (
     <StyledSection>
       <div className="wrapper">
-        <h1>ContentTwoColumns</h1>
+        <div className="main-title">
+          <h2>{data.mainTitle}</h2>
+        </div>
+        <div className="column-content">
+          <div className="sub-title">
+            <h3>{data.leftSubTitle}</h3>
+          </div>
+          <div
+            className="blue-content"
+            dangerouslySetInnerHTML={{ __html: data.leftBlueContent }}
+          />
+        </div>
+        <div className="column-content">
+          <div
+            className="main-content"
+            dangerouslySetInnerHTML={{ __html: data.rightContent }}
+          />
+        </div>
       </div>
     </StyledSection>
   )
 }
 
-const StyledSection = styled.section``
+const StyledSection = styled.section`
+  padding: 9rem 0;
+  .wrapper {
+    ${medWrapper};
+  }
+
+  .main-title {
+    position: relative;
+    margin-bottom: 4rem;
+    padding-bottom: 5rem;
+    width: 100%;
+
+    &::after {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 17.7rem;
+      height: 0.9rem;
+      background-color: ${colors.colorPrimary};
+      content: "";
+    }
+
+    h2 {
+      ${H1Black};
+      margin: 0;
+      text-transform: uppercase;
+    }
+  }
+
+  .column-content {
+    width: calc(100%);
+
+    @media (min-width: 768px) {
+      width: calc(50% - 2rem);
+    }
+
+    .sub-title {
+      h3 {
+        ${H2Black};
+        margin-top: 0;
+        font-weight: normal;
+      }
+    }
+
+    .blue-content {
+      p {
+        ${H3Black};
+        font-weight: normal;
+        color: ${colors.colorPrimary};
+      }
+    }
+
+    .main-content {
+      p {
+        ${B1Black};
+      }
+    }
+
+    &:first-of-type {
+      @media (min-width: 768px) {
+        margin-right: 2rem;
+      }
+    }
+
+    &:last-of-type {
+      @media (min-width: 768px) {
+        margin-left: 2rem;
+      }
+    }
+  }
+`
 
 export default ContentTwoColumns
