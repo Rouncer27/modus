a4644cf820c366c9b7a4916b954e7fcce060495a
Trevor Rounce <trevor@switchbackcreative.ca>
2025-08-26 13:08:00 -0600

    added script code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,6 +7,31 @@
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({
+  setHtmlAttributes,
+  setHeadComponents,
+  setPostBodyComponents,
+}) => {
+  // keep your existing lang attribute ✅
   setHtmlAttributes({ lang: `en` })
+
+  // 🔵 Added tracking script to <head>
+  setHeadComponents([
+    <script
+      key="groupcreative365-script"
+      type="text/javascript"
+      src="https://www.groupcreative365.com/js/806860.js"
+    />,
+  ])
+
+  // 🔵 Added <noscript> fallback to <body>
+  setPostBodyComponents([
+    <noscript key="groupcreative365-noscript">
+      <img
+        alt=""
+        src="https://www.groupcreative365.com/806860.png"
+        style={{ display: "none" }}
+      />
+    </noscript>,
+  ])
 }
