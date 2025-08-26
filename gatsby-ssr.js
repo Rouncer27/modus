be2f5a1a746dbe23cabbd170efc6b2efab46e254
Trevor Rounce <trevor@switchbackcreative.ca>
2025-08-26 13:14:20 -0600

    added script tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,4 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
-
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
+import React from "react"
 exports.onRenderBody = ({
   setHtmlAttributes,
   setHeadComponents,
