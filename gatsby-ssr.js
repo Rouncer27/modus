3b27414ecffbce4fa283bb35a2c4241457353e5b
Trevor Rounce <trevor@switchbackcreative.ca>
2025-08-26 13:15:47 -0600

    added script tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,25 +1,32 @@
-import React from "react"
+const React = require("react") // 🟣 import React since JSX won't work in Node
+
 exports.onRenderBody = ({
   setHtmlAttributes,
   setHeadComponents,
   setPostBodyComponents,
 }) => {
+  // keep your existing lang attribute
   setHtmlAttributes({ lang: `en` })
 
+  // tracking script in <head>
   setHeadComponents([
-    <script
-      key="groupcreative365-script"
-      type="text/javascript"
-      src="https://www.groupcreative365.com/js/806860.js"
-    />,
+    React.createElement("script", {
+      key: "groupcreative365-script",
+      type: "text/javascript",
+      src: "https://www.groupcreative365.com/js/806860.js",
+    }),
   ])
+
+  // noscript fallback in <body>
   setPostBodyComponents([
-    <noscript key="groupcreative365-noscript">
-      <img
-        alt=""
-        src="https://www.groupcreative365.com/806860.png"
-        style={{ display: "none" }}
-      />
-    </noscript>,
+    React.createElement(
+      "noscript",
+      { key: "groupcreative365-noscript" },
+      React.createElement("img", {
+        alt: "",
+        src: "https://www.groupcreative365.com/806860.png",
+        style: { display: "none" },
+      })
+    ),
   ])
 }
